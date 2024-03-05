e1bfde99bd8d68092e3e4be42d3272cfc1934b5d
Vignesh <vigneshe04@gmail.com>
2024-03-05 17:20:22 +0530

    Cart,Routing,Store partially completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,11 @@ import { SKY_LOGO } from "../constants";
 
 const Header = () => {
   return (
-    <div className="p-2 bg-fuchsia-200">
+    <div className="p-1 bg-fuchsia-200 flex justify-between">
       <div className="m-2 py-2 pl-2">
         <img src={SKY_LOGO} alt="sky-logo" className="w-20 h-10" />
       </div>
+      <h1 className="m-5 mr-10 h-8 p-1 cursor-pointer border-2 border-solid rounded-lg border-stone-400 font-sans font-bold text-2xl flex items-center hover:bg-purple-300">Cart 🛒</h1>
     </div>
   );
 };
--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -9,13 +9,13 @@ const SongCard = ({ data }) => {
     formattedDate = formattedDate.toLocaleDateString("en-US", {
       day: "2-digit",
       month: "short",
-      year: "numeric",
+      year: "2-digit",
     });
     return formattedDate;
   };
 
   return (
-    <div className="m-3 w-44 h-60 border- border-solid border-purple-300 rounded-lg bg-purple-300">
+    <div className="m-3 w-44 h-64 cursor-pointer border border-solid border-stone-400 rounded-lg bg-purple-200 hover:bg-purple-300">
       <img
         className="m-2 w-40 h-28 rounded-md border border-solid"
         src={data && data["im:image"]?.[2]?.label}
@@ -34,9 +34,12 @@ const SongCard = ({ data }) => {
             " " +
             data["im:price"]?.attributes?.currency}
       </p>
-      <p className="m-1 text-xs font-semibold flex items-start">
+     <div className="flex justify-between items-center">
+     <p className="m-1 text-xs flex font-semibold items-start">
         Out on {data && formatDate(data["im:releaseDate"]?.label)}
       </p>
+      <button className="w-12 mr-2 bg-orange-200 border-stone-500 h-5 cursor-pointer border border-solid rounded-md text-xs font-semibold hover:bg-orange-500">Buy🛒</button>
+     </div>
     </div>
   );
 };
