556f38323ee07057a89a27821eda54f3d40d13ef
Vignesh <vigneshe04@gmail.com>
2024-03-12 22:16:54 +0530

    Added Cart value Functionality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,11 +5,30 @@ import { clearSongs } from "./store/CartSlice";
 
 const Cart = () => {
   const items = useSelector((store) => store?.cart?.songs);
+  const cartValue = useSelector((store) => store?.cart?.value);
   const dispatch = useDispatch();
+
+  let totalCartValue = cartValue.reduce((sum, item) => {
+    let val = sum + item;
+    return val;
+  }, 0);
+  console.log(totalCartValue, "cartval");
 
   return (
     <div className="bg-fuchsia-200 ">
-      {items?.length !== 0 && <button className="p-1 w-40 border-2 border-stone-500 rounded-3xl" onClick={()=>dispatch(clearSongs())}>Clear Cart</button>}
+      <div className="flex justify-center">
+        {items?.length !== 0 && (
+          <button
+            className="p-1 w-40 border-2 border-stone-500 rounded-3xl"
+            onClick={() => dispatch(clearSongs())}
+          >
+            Clear Cart
+          </button>
+        )}
+        <div className="ml-56 mr-10 p-1 w-52 border font-bold text-lg absolute right-0 flex justify-center border-stone-500 rounded-lg bg-orange-400">
+          Cart Value : $ {totalCartValue.toFixed(2) || "0"}
+        </div>
+      </div>
       <div className="pt-3 h-screen flex flex-wrap">
         {items.length === 0 ? (
           <div className="h-8 p-10 ml-60 w-3/5 border border-stone-400 rounded-3xl flex justify-center items-center font-bold text-xl">
--- src/components/store/CartSlice.js
+++ src/components/store/CartSlice.js
@@ -4,10 +4,12 @@ const CartSlice = createSlice({
   name: "cart",
   initialState: {
     songs: [],
+    value: [],
   },
   reducers: {
     addSong: (state, action) => {
       state.songs.push(action.payload);
+      state.value.push(Number(action.payload["im:price"]?.attributes?.amount))
     },
     removeSong: (state, action) => {
       state.songs = state?.songs?.filter(
